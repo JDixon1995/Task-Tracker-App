8126e081c2aabd7b4f4a5a2a56814e1490f71ff0
John <jddhalogamer@gmail.com>
2021-10-19 16:46:10 -0500

    refactored Tasks with a Task component, and brought task list up the a global scope for useState.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks';
 
 const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Wash car.' ,
+        day: 'Nov 1st at 2:30 PM',
+        reminder: false,
+    },
+    {
+        id: 2,
+        text: 'Go to gym class',
+        day: 'Nov 4th at 5:30 PM',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Help son finish homework',
+        day: 'Nov 7th at 6:30 PM',
+        reminder: true,
+    }
+])
+
   return (
-    <div className="App" className="container">
+    <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,13 @@
 import React from 'react'
+import Task from './Task'
 
-const tasks = [
-    {
-        id: 1,
-        text: 'Wash car.' ,
-        day: 'Nov 1st at 2:30 PM',
-        reminder: false,
-    },
-    {
-        id: 2,
-        text: 'Go to gym class',
-        day: 'Nov 4th at 5:30 PM',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Help son finish homework',
-        day: 'Nov 7th at 6:30 PM',
-        reminder: true,
-    }
-]
 
-const Tasks = () => {
+
+const Tasks = ( {tasks} ) => {
+    
     return (
         <>
-            {tasks.map((task) => (<h3 key={task.id} >{task.text}</h3>))}
+            {tasks.map((task) => (<Task key={task.id} task={task} /> ))}
         </>
     )
 }
