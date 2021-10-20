553db8277e4ae149114ed74ed8a8df67f7dcfbac
John <jddhalogamer@gmail.com>
2021-10-20 16:11:33 -0500

    added functionality to add a task object to the App state object.

--- src/App.js
+++ src/App.js
@@ -27,6 +27,14 @@ const App = () => {
     }
 ])
 
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+
+  const newTask = {id, ...task }
+  setTasks([...tasks, newTask])
+}
+
  //Delete Task
  const deleteTask = (id) => {
    setTasks(tasks.filter((task) => task.id !== id))
@@ -45,7 +53,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (<Tasks tasks={tasks} 
       onDelete={deleteTask} 
       onToggle={toggleReminder} />) : 
