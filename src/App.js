df865f4683c8aacb40f4e73961f92ad861bb28ae
John <jddhalogamer@gmail.com>
2021-10-21 17:23:54 -0500

    app now has functionality to toggle reminder highlight on dbl-click, and updates data on the back-end.

--- src/App.js
+++ src/App.js
@@ -18,9 +18,17 @@ const App = () => {
     getTasks()
   }, [])
 
-  //Fetch tasks
+  //Fetch Tasks
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+
+  //Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
     const data = await res.json()
 
     return data
@@ -31,14 +39,14 @@ const addTask = async (task) => {
   const res = await fetch('http://localhost:5000/tasks', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(task)
+    body: JSON.stringify(task),
   })
 
-  const data = res.json()
+  const data = await res.json()
 
-  setTasks([...tasks], data)
+  setTasks([...tasks, data])
   /* const id = Math.floor(Math.random() * 10000) + 1
 
   const newTask = {id, ...task }
@@ -54,11 +62,25 @@ const addTask = async (task) => {
  }
 
  // Toggle Reminder 
- const toggleReminder = (id) => {
+ const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updatedTask = {...taskToToggle, 
+  reminder: !taskToToggle.reminder }
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, { 
+    method: 'PUT', 
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedTask)
+  })
+
+  const data = await res.json()
+
    setTasks(
      tasks.map((task) => 
      task.id === id ? {...task, reminder: 
-      !task.reminder } : task
+      data.reminder } : task
     )
    )
  }
