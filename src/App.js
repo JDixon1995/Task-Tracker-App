a28f32ad632df3824672b857e5707feec34956a1
John <jddhalogamer@gmail.com>
2021-10-21 16:55:08 -0500

    app now fetches data from the back-end to display on the front-end with the useEffect hook.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
@@ -9,6 +9,22 @@ const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, [])
+
+  //Fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
 
 // Add Task
 const addTask = (task) => {
