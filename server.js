9232e5c4b74c12419cd15c7ccbac3c320d6cca9f
John <jddhalogamer@gmail.com>
2022-01-03 13:50:26 -0600

    defined root get route for back-end app.

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const cors = require('cors')
-const db = require('db.json')
+
+const tasks = require('./db.json')
 
 const app = express()
 app.use(cors())
 
+const PORT = process.env.PORT || 5000
 
+app.listen(PORT)
+
+console.log(`App listening on port ${PORT}`)
+
+app.get('/', (req, res) => {
+    console.log('Accessed Root Route')
+    res.json(tasks)
+})
 
 app.use('*', (req, res) => {
 res.status(404).json({error: "Page not found"})
